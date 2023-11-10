54799ce0ddc0d46dc19d8bde17693dd308cfb0a4
Eneias S Brum Jr <eneiassbrumjr@gmail.com>
2023-11-10 13:18:01 -0300

    Added some new codes and validations

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
-@section('title', 'Add Task')
+@section('title', 'Edit Task')
 
 @section('styles')
     <style>
         .error-message {
             color: red;
-            font-size: 0.8rem;
+            font-size: 0, 8rem;
         }
     </style>
 
 @section('content')
-    <form method="POST" action="{{ route('tasks.store') }}">
+    <form method="POST" action="{{ route('tasks.update', ['task'=> $task->id ]) }}">
         @csrf
+        @method('PUT')
         <div>
             <label for="title">
                 Title
             </label>
-            <input type="text" name="title" id="title" value="{{ old ('title') }}" />
+            <input type="text" name="title" id="title" value="{{ $task->title }}" />
         </div>
         @error('title')
             <p class="error-message">{{ $message }}</p>
@@ -27,7 +28,7 @@
             <label for="description">
                 Description
             </label>
-            <textarea name="description" id="description" rows="5">{{ old ('description') }}</textarea>
+            <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
         </div>
         @error('description')
             <p class="error-message">{{ $message }}</p>
@@ -37,14 +38,14 @@
             <label for="long_description">
                 Long Description
             </label>
-            <textarea name="long_description" id="long_description" rows="10">{{ old ('long_description') }}</textarea>
+            <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
         </div>
         @error('long_description')
             <p class="error-message">{{ $message }}</p>
         @enderror
 
         <div>
-            <input type="submit" value="Add Task" />
+            <input type="submit" value="Edit Task" />
         </div>
     </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
@@ -21,33 +22,35 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => \App\Models\Task::latest()->where('completed', true)->get()
+        'tasks' => Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) {
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
-        'task' => \App\Models\Task::findOrFail($id)
+        'task' => $task
     ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function (Request $request){
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
+Route::post('/tasks', function (TaskRequest $request){
+    $task = Task::create($request->validated());
 
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    $task->save();
-
-    return redirect()->route('tasks.show', ['id' => $task->id])
+    return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/${task}', function (Task $task, TaskRequest $request){
+    $task->update($request->validated());
+
+    return redirect()->route('tasks.show', ['task' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
